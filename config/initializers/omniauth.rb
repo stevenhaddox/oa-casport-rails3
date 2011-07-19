90269e3a6b5535a7e9289eb19e8674e147a14bf1
Steven Haddox <steven@haddox.us>
2011-07-19 00:28:18 -0400

    debug commit for the night

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :casport, {
     :setup => true,
-    :cas_server => 'http://oa-cas.slkdemos.com/users/'
+    :cas_server => 'http://cas.dev/users'
+    #:cas_server => 'http://oa-cas.slkdemos.com/users'
   }
 end
